baae025b285e0cba6f8b995094233b995d196f9e
George <machadogeorge.p@gmail.com>
2022-02-27 22:28:41 -0300

    First commit

---
+++ src/test/java/com/softbox/voteapi/VoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softbox.voteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
